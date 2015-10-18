f610ed66d5ad27d3a539e4b3eefabe2d5974754d
Thiago Paes <mrprompt@gmail.com>
2015-10-18 13:22:24 -0200

    refactoring e PSR2 aplicados

--- lib/Palma/Silex/Provider/DoctrineORMServiceProvider.php
+++ lib/Palma/Silex/Provider/DoctrineORMServiceProvider.php
@@ -35,57 +35,74 @@ use Silex\Application;
 use Silex\ServiceProviderInterface;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\Configuration;
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\Common\EventManager;
-use Doctrine\Common\Cache\ArrayCache;
+use Doctrine\Common\Cache\ArrayCache as Cache;
 
-class DoctrineORMServiceProvider implements ServiceProviderInterface {
-    public function register(Application $app){
-        $app['doctrine_orm.configuration'] = $app->share(function($app){
+/**
+ * Class DoctrineORMServiceProvider
+ * @package Palma\Silex\Provider
+ */
+class DoctrineORMServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * Register ORM on Application
+     *
+     * @param Application $app
+     */
+    public function register(Application $app)
+    {
+        $app['doctrine_orm.configuration'] = $app->share(function ($app) {
             $configuration = new Configuration();
-            if(isset($app['doctrine_orm.metadata_cache'])){
-	            $configuration->setMetadataCacheImpl($app['doctrine_orm.metadata_cache']);
-	        } else {
-	        	$configuration->setMetadataCacheImpl(new ArrayCache());
-	        }
-            $driverImpl = $configuration->newDefaultAnnotationDriver($app['doctrine_orm.entities_path'], boolval($app['doctrine_orm.simple_annotation_reader']));
+
+            $mCache = isset($app['doctrine_orm.metadata_cache']) ? $app['doctrine_orm.metadata_cache'] : new Cache();
+            $qCache = isset($app['doctrine_orm.query_cache']) ? $app['doctrine_orm.query_cache'] : new Cache();
+            $rCache = isset($app['doctrine_orm.result_cache']) ? $app['doctrine_orm.result_cache'] : new Cache();
+
+            $annotation = boolval($app['doctrine_orm.simple_annotation_reader']);
+            $driverImpl = $configuration->newDefaultAnnotationDriver($app['doctrine_orm.entities_path'], $annotation);
+
+            $configuration->setMetadataCacheImpl($mCache);
             $configuration->setMetadataDriverImpl($driverImpl);
-
-            if(isset($app['doctrine_orm.query_cache'])){
-	            $configuration->setQueryCacheImpl($app['doctrine_orm.query_cache']);
-	        } else {
-	        	$configuration->setQueryCacheImpl(new ArrayCache());
-            }
-
-            if(isset($app['doctrine_orm.result_cache'])){
-                $configuration->setResultCacheImpl($app['doctrine_orm.result_cache']);
-            }
-
+            $configuration->setQueryCacheImpl($qCache);
+            $configuration->setResultCacheImpl($rCache);
             $configuration->setProxyDir($app['doctrine_orm.proxies_path']);
             $configuration->setProxyNamespace($app['doctrine_orm.proxies_namespace']);
             $configuration->setAutogenerateProxyClasses(false);
-            if(isset($app['doctrine_orm.autogenerate_proxy_classes'])){
+
+            if (isset($app['doctrine_orm.autogenerate_proxy_classes'])) {
                 $configuration->setAutogenerateProxyClasses($app['doctrine_orm.autogenerate_proxy_classes']);
-			} else {
-				$configuration->setAutogenerateProxyClasses(true);
-			}
+            } else {
+                $configuration->setAutogenerateProxyClasses(true);
+            }
+
             return $configuration;
         });
 
-        $app['doctrine_orm.connection'] = $app->share(function($app){
-            return DriverManager::getConnection($app['doctrine_orm.connection_parameters'], $app['doctrine_orm.configuration'], new EventManager());
+        $app['doctrine_orm.connection'] = $app->share(function ($app) {
+            return DriverManager::getConnection(
+                $app['doctrine_orm.connection_parameters'],
+                $app['doctrine_orm.configuration'],
+                new EventManager()
+            );
         });
 
-        $app['doctrine_orm.em'] = $app->share(function($app) {
-            return EntityManager::create($app['doctrine_orm.connection'], $app['doctrine_orm.configuration'], $app['doctrine_orm.connection']->getEventManager());
+        $app['doctrine_orm.em'] = $app->share(function ($app) {
+            return EntityManager::create(
+                $app['doctrine_orm.connection'],
+                $app['doctrine_orm.configuration'],
+                $app['doctrine_orm.connection']->getEventManager()
+            );
         });
-		
-    }
-
-    public function boot(Application $app) {
         
     }
-}
 
-?>
+    /**
+     * Boot
+     * @param Application $app
+     */
+    public function boot(Application $app)
+    {
+        // do nothing
+    }
+}
